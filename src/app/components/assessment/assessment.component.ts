8176501ed22e15e6a5e94372541b8d81e9190057
lorenz <lorenz.bre@gmail.com>
2020-11-22 11:46:20 +0100

    Fixed Information Modal (change to ng-bootstrap)

--- src/app/components/assessment/assessment.component.ts
+++ src/app/components/assessment/assessment.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Assessment } from '../../models/Assessment';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-assessment',
@@ -10,9 +12,28 @@ export class AssessmentComponent implements OnInit {
 
   @Input() assessment: Assessment;
 
-  constructor() { }
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+  open(content): void {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
